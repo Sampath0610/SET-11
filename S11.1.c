9301703bcd59744a4f54a9dc148ad2af76b1bc19
Sampath0610 <46046788+Sampath0610@users.noreply.github.com>
2019-02-11 15:44:05 +0530

    Add files via upload

---
+++ S11.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,a1,a2;
+    printf("enter the number");
+    scanf("%d",&n);
+    while(a==0)
+    {
+        a=a/10;
+        a2=a;
+        a1=+a;
+        
+    }
+    printf("value :",a);
+}
